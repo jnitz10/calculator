b95c0cb0b9a2d7396f821240a6a026edf3f55f2e
Jnitz10 <jnitz10@gmail.com>
2021-11-14 14:10:39 -0700

    commit

--- app.js
+++ app.js
@@ -4,6 +4,70 @@
 
 // del.addEventListener('mousedown', function() {console.log("sdf")});
 
+const keyVals = [
+    {
+        keyName: 'one',
+        keyVal: '1'
+    },
+    {
+        keyName: 'two',
+        keyVal: '2'
+    },
+    {
+        keyName: 'three',
+        keyVal: '3'
+    },
+    {
+        keyName: 'four',
+        keyVal: '4'
+    },
+    {
+        keyName: 'five',
+        keyVal: '5'
+    },
+    {
+        keyName: 'six',
+        keyVal: '6'
+    },
+    {
+        keyName: 'seven',
+        keyVal: '7'
+    },
+    {
+        keyName: 'eight',
+        keyVal: '8'
+    },
+    {
+        keyName: 'nine',
+        keyVal: '9'
+    },
+    {
+        keyName: 'zero',
+        keyVal: '0'
+    },
+    {
+        keyName: 'decimal',
+        keyVal: '.'
+    },
+    {
+        keyName: 'add',
+        keyVal: '+'
+    },
+    {
+        keyName: 'subtract',
+        keyVal: '-'
+    },
+    {
+        keyName: 'multiply',
+        keyVal: 'x'
+    },
+    {
+        keyName: 'divide',
+        keyVal: '÷'
+    }
+]
+
+
 function add(x, y) {
     return Number(x) + Number(y);
 }
@@ -36,6 +100,16 @@ function operate(x, op, y) {
 const topLine = document.getElementById('topLine');
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('click', function() {
-    console.log(key.innerHTML);
-    topLine.innerHTML += key.innerHTML;
+    console.log(key.id);
+    topLine.innerHTML += key.id;
     }));
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', function() {
+    topLine.innerHTML = '';
+});
+
+const del = document.getElementById('delete');
+del.addEventListener('click', function() {
+    topLine.innerHTML = topLine.innerHTML.slice(0,-1);
+});
