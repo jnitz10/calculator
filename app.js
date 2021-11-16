f869188fcddbd9289906bc9db2020273f70b9496
Jnitz10 <jnitz10@gmail.com>
2021-11-15 18:13:31 -0700

    commit

--- app.js
+++ app.js
@@ -97,6 +97,32 @@ function operate(x, op, y) {
     }
 
 }
+
+class Stack {
+    constructor(){
+        this.data = [];
+        this.top = 0;
+    }
+    push(element) {
+        this.data[this.top] = element;
+        this.top = this.top + 1;
+    }
+    length() {
+        return this.top;
+    }
+    peek() {
+        return this.data[this.top - 1];
+    }
+    isEmpty() {
+        return this.top === 0;
+    }
+    pop() {
+        if(this.isEmpty() === false) {
+            this.top = this.top - 1;
+            return this.data.pop(); 
+        }
+    }
+}
 const topLine = document.getElementById('topLine');
 const keys = Array.from(document.querySelectorAll('.key'));
 keys.forEach(key => key.addEventListener('click', function() {
